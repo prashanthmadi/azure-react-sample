a49bd46c1a67ee9ae6ef80354f0032f21375e4b1
Prashanth Madi <prashanthrmadi@gmail.com>
2017-09-11 22:52:56 -0500

    :bug: unable to select node version. selecting default node version

--- deploy.sh
+++ deploy.sh
@@ -63,35 +63,6 @@ if [[ ! -n "$KUDU_SYNC_CMD" ]]; then
     KUDU_SYNC_CMD=$APPDATA/npm/node_modules/kuduSync/bin/kuduSync
   fi
 fi
-# Node Helpers
-# ------------
-
-selectNodeVersion () {
-  if [[ -n "$KUDU_SELECT_NODE_VERSION_CMD" ]]; then
-    SELECT_NODE_VERSION="$KUDU_SELECT_NODE_VERSION_CMD \"$DEPLOYMENT_SOURCE{SitePath}\" \"$DEPLOYMENT_TARGET\" \"$DEPLOYMENT_TEMP\""
-    eval $SELECT_NODE_VERSION
-    exitWithMessageOnError "select node version failed"
-
-    if [[ -e "$DEPLOYMENT_TEMP/__nodeVersion.tmp" ]]; then
-      NODE_EXE=`cat "$DEPLOYMENT_TEMP/__nodeVersion.tmp"`
-      exitWithMessageOnError "getting node version failed"
-    fi
-    
-    if [[ -e "$DEPLOYMENT_TEMP/__npmVersion.tmp" ]]; then
-      NPM_JS_PATH=`cat "$DEPLOYMENT_TEMP/__npmVersion.tmp"`
-      exitWithMessageOnError "getting npm version failed"
-    fi
-
-    if [[ ! -n "$NODE_EXE" ]]; then
-      NODE_EXE=node
-    fi
-
-    NPM_CMD="\"$NODE_EXE\" \"$NPM_JS_PATH\""
-  else
-    NPM_CMD=npm
-    NODE_EXE=node
-  fi
-}
 
 ##################################################################################################################################
 # Deployment
@@ -99,22 +70,19 @@ selectNodeVersion () {
 
 echo Handling react app deployment.
 
-# 1. Select node version
-selectNodeVersion
-
-# 2. Install npm packages
+# 1. Install npm packages
 if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
   cd "$DEPLOYMENT_SOURCE"
-  echo "Running $NPM_CMD install"
-  eval $NPM_CMD install
+  echo "Running npm install"
+  eval npm install
   exitWithMessageOnError "npm failed"
   echo "Building react app"
-  eval $NPM_CMD run build
+  eval npm run build
   exitWithMessageOnError "react build failed"
  cd - > /dev/null
 fi
 
-# 3. KuduSync
+# 2. KuduSync
 if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
   "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE{SitePath}/build" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
   exitWithMessageOnError "Kudu Sync failed"
